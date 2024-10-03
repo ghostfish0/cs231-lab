4770672c1ff19d7876bc23fab47feda82fdf2725
Tin Nguyen <nguyenvb.tin03@gmail.com>
2024-10-03 10:57:56 -0400

    fixed up landscape.java

--- w3/Landscape.java
+++ w3/Landscape.java
@@ -148,8 +148,8 @@ public class Landscape {
 			for (int y : offsets) {
 				if (x == 0 && y == 0)
 					continue;
-				int nRow = row + pair[0];
-				int nCol = col + pair[1];
+				int nRow = row + x;
+				int nCol = col + y;
 				if ((nRow < 0 || nRow >= this.landscape.length) ||
 				    (nCol < 0 || nCol >= this.landscape[0].length))
 					continue;
@@ -165,12 +165,12 @@ public class Landscape {
 	public void advance() {
 		int rows = this.landscape.length;
 		int cols = this.landscape[0].length;
-        // init the temporary, editable landscape
+		// init the temporary, editable landscape
 		Cell[][] workLandscape = new Cell[rows][cols];
 
-        // remove the oldest historicalLandscape from the head of the queue
+		// remove the oldest historicalLandscape from the head of the queue
 		this.historicalLandscapes.poll();
-        // add the latest (current) landscape to the tail of the queue
+		// add the latest (current) landscape to the tail of the queue
 		this.historicalLandscapes.offer(this.landscape);
 
 		// creates a copy of the currentLandscape;
@@ -179,13 +179,13 @@ public class Landscape {
 				workLandscape[row][col] = new Cell(this.landscape[row][col].getAlive());
 			}
 		}
-        // update the temporary work landscape
+		// update the temporary work landscape
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
 				workLandscape[row][col].updateState(getNeighbors(row, col));
 			}
 		}
-        // update the current landscape
+		// update the current landscape
 		this.landscape = workLandscape;
 	}
 
@@ -193,10 +193,10 @@ public class Landscape {
 	 * Set the Cell equivalent to point (x,y) to Alive
 	 *
 	 * @param x The x (horizontal) position relative to the window's width, range from 0.0 to 1.0
-     * @param y The y (vertical) position relative to the window's height, range from 0.0 to 1.0
+	 * @param y The y (vertical) position relative to the window's height, range from 0.0 to 1.0
 	 */
-	public void setCellAlive(double , double y) {
-        int row = (int)Math.floor(x * getRows());
+	public void setCellAlive(double x, double y) {
+		int row = (int)Math.floor(x * getRows());
 		int col = (int)Math.floor(y * getCols());
 		if (row < 0 || row >= getRows() || col < 0 || col > getCols())
 			return;
@@ -206,7 +206,7 @@ public class Landscape {
 	/**
 	 * Count how many alive cells are there on the board
 	 *
-     * @return The number of alive cells on the board
+	 * @return The number of alive cells on the board
 	 */
 	public int getSum() {
 		return Arrays.stream(this.landscape).flatMap(Arrays::stream).mapToInt(i -> i.getAlive() ? 1 : 0).sum();
@@ -215,10 +215,10 @@ public class Landscape {
 	/**
 	 * Helper function to draw a given Cell[][] grid
 	 *
-     * @param g the Java Graphics to draw to 
-     * @param scale size of each cell
-     * @param scape the Cell[][] grid to be drawn
-     * @param color color of the scape
+	 * @param g the Java Graphics to draw to
+	 * @param scale size of each cell
+	 * @param scape the Cell[][] grid to be drawn
+	 * @param color color of the scape
 	 */
 	public void drawScape(Graphics g, int scale, Cell[][] scape, Color color) {
 		g.setColor(color);
@@ -240,7 +240,9 @@ public class Landscape {
 	 */
 	public void draw(Graphics g, int scale) {
 		int redness = 0;
-		for (Cell[][] scape : historicalLandscapes) {
+		// Copy of historicalLandscapes to avoid For loop modification error
+		ArrayList<Cell[][]> historicalLandscapesCopy = new ArrayList<>(historicalLandscapes);
+		for (Cell[][] scape : historicalLandscapesCopy) {
 			if (scape[0][0] == null)
 				continue;
 			redness = (redness + 10 < 255 ? redness + 10 : 255);
